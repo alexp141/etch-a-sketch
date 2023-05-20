b4069ae58cd51dc1c491f80ba716b9dda277dec5
Alexander Pastor <apastor@umd.edu>
2023-05-20 18:12:40 -0400

    grid working as intended

--- script.js
+++ script.js
@@ -1,36 +1,81 @@
 function createGrid(size=16) {
     let gridContainer = document.querySelector('.grid-container');
     gridContainer.innerHTML = null;
-    
+
     for (let i = 0; i < size; i++) {
         let rowContainer = document.createElement('div');
         rowContainer.classList.add('row');
         rowContainer.style.cssText = 'display: flex; width: 100%';
+
         for (let j = 0; j < size; j++) {
             let cellContainer = document.createElement('div');
-            cellContainer.style.cssText = 'background-color: white; border: 1px solid gray; margin: -1px; flex: 1;';
-            
+            cellContainer.classList.add('cell');
+            cellContainer.style.cssText = 'background-color: white; margin: -1px; flex: 1;';
+
             cellContainer.addEventListener('mouseenter', e => {
-                e.target.style.cssText = 'background-color: grey; border: 1px solid gray; margin: -1px; flex: 1;';
-            })
-            cellContainer.addEventListener('mouseleave', e => {
-                e.target.style.cssText = 'background-color: white; border: 1px solid gray; margin: -1px; flex: 1;';
-            })
+                if (e.buttons > 0) {
+                    e.target.style.cssText = `background-color: black; margin: -1px; flex: 1;`;
+                }
+            });
             
+            cellContainer.addEventListener('mousedown', e => {
+                e.target.style.cssText = `background-color: black; margin: -1px; flex: 1;`;
+            });
+
             rowContainer.appendChild(cellContainer);
         }
         gridContainer.appendChild(rowContainer);
     }
 }
 
-function addSliderListener() {
+function addSliderConfig() {
     let slider = document.getElementById('slider')
     slider.addEventListener('change', e => {
         let newVal = e.target.valueAsNumber;
        console.log(newVal);
        createGrid(newVal); 
     });
+
+    slider.oninput = function() {
+        document.querySelector('.size').innerHTML = `${this.value} x ${this.value}`;
+      }
 }
 
-createGrid();
-addSliderListener();
+
+function main() {
+    
+    createGrid();
+    addSliderConfig();
+
+    let color = document.getElementById('color-picker');
+    let colorVal = color.value ?? 'black';
+    let currColor = colorVal;
+
+    color.addEventListener('change', e => {
+        currColor = e.target.value;
+
+        let cellList = document.querySelectorAll('.cell');
+
+        cellList.forEach(element => {
+            element.removeEventListener('mousedown', e => {});
+            element.removeEventListener('mouseenter', e => {});
+            
+
+            element.addEventListener('mouseenter', e => {
+                if (e.buttons > 0) {
+                    e.target.style.cssText = `background-color: ${currColor}; margin: -1px; flex: 1;`;
+                }
+            });
+            
+            element.addEventListener('mousedown', e => {
+                e.target.style.cssText = `background-color: ${currColor}; margin: -1px; flex: 1;`;
+            });
+
+            
+        });
+    });
+
+    console.log(colorVal);
+}
+
+main();
